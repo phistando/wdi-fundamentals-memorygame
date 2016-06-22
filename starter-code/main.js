95bf8f85f5988e40858620b52e2e47bbcb884e81
Sebastian Sim <sebastiansim@Sebastians-MacBook-Pro.local>
2016-06-22 20:16:48 +0800

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,10 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+
+if (cardOne === cardTwo || cardThree === cardFour) {
+	alert('You found a match!');	
+} else {
+  alert('Sorry, try again.');
+}
